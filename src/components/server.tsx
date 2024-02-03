721ed16dc447b124d2692a018ebd636095f3778f
Luca Ignatescu <lucaignatescu003@gmail.com>
2024-02-02 23:04:33 -0500

    Simple skeleton

---
+++ src/components/server.tsx
@@ -0,0 +1,20 @@
+export const Header = () =>
+(<div className="py-20 flex flex-row justify-between bg-red-50" >
+  <span></span>
+  <div className="px-20 text-l inline-block border-black border-solid rounded-md border-0">
+    <span className="px-5">
+      Resume
+    </span>
+    <span className="px-5">
+      Github
+    </span>
+    <span className="px-5">
+      LinkdIn
+    </span>
+    <span className="line-through px-5">
+      Blog
+    </span>
+  </div>
+</div>)
+
+
