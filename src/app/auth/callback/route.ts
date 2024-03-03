a0d7a884169e4013e3bc9729b283544322bf9e1e
Luca Ignatescu <lucaignatescu003@gmail.com>
2024-03-03 13:52:11 -0500

    login+ token issuing

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -1,6 +1,21 @@
+import { dbConnect } from "@/utils/postgres";
 import { redirect } from "next/navigation";
 import { NextRequest } from "next/server";
-import postgres from "postgres";
+import { SignJWT } from "jose";
+
+async function getUsername(token: string) {
+  const data = await fetch("https://api.github.com/user", {
+    headers: {
+      "Accept": "application/vnd.github+json",
+      "Authorization": "Bearer " + token,
+      "X-GitHub-Api-Version":
+        "2022-11-28"
+    },
+    cache: 'no-store'
+  }).then(res => res.json());
+
+  return data.login;
+}
 
 export async function GET(req: NextRequest) {
   const code = req.nextUrl.searchParams.get('code')!;
@@ -8,19 +23,39 @@ export async function GET(req: NextRequest) {
   url.searchParams.append("client_id", process.env.GITHUB_CLIENT!);
   url.searchParams.append("client_secret", process.env.GITHUB_SECRET!);
   url.searchParams.append("code", code);
-  console.log("hit");
 
   const data = await fetch(url.toString(), { cache: 'no-store' }).then(res => res.formData())
-  const access_token = data.get('access_token')?? "";
-  const refresh_token = data.get('refresh_token')?? "";
+  const access_token = data.get('access_token') as string;
+  const refresh_token = data.get('refresh_token') as string;
+  const username = await getUsername(access_token);
+  const sql = dbConnect();
+  const secret = new TextEncoder().encode(process.env.TOKEN_SECRET);
+  const alg = 'HS256';
 
-  const userData = await fetch('https://api.github.com/users/LucaIgnatescu/repos', {
-    headers:{
-      'Accept': 'application/vnd.github+json',
-      'X-GitHub-Api-Version': '2022-11-28',
-      'Authorization': 'Bearer ' + access_token
-    }
-  });
-  console.log(await userData.json())
+  const access = await new SignJWT({ sub: username })
+    .setProtectedHeader({ alg })
+    .setIssuedAt()
+    .setExpirationTime("5minutes")
+    .sign(secret);
+
+  const refresh = await new SignJWT({ sub: username })
+    .setProtectedHeader({ alg })
+    .setIssuedAt()
+    .setExpirationTime("2weeks")
+    .sign(secret);
+
+  try {//TODO: OPTIMIZE QUERY
+    await sql`
+      INSERT INTO TOKENS (token) VALUES (${refresh}) returning token;
+      `;
+    const user = await sql`
+      INSERT INTO USERS (username,refresh_token,github_access_token, github_refresh_token) VALUES
+      (${username},(SELECT token from tokens where token=${refresh}), ${access_token}, ${refresh_token});
+      `;
+    console.log(user);
+  }
+  catch (err) {
+    console.log(err);
+  }
   redirect('/auth');
 }
--- src/utils/postgres.ts
+++ src/utils/postgres.ts
@@ -1,15 +1,15 @@
 import postgres, { Sql } from 'postgres';
 
-let instance:null | Sql = null;
+let sql: null | Sql = null;
 
-export default function dbConnect() {
-  if (instance === null) {
-    instance = postgres({
+export function dbConnect() {
+  if (sql === null) {
+    sql = postgres({
       host: process.env.DB_ENDPOINT,
       database: process.env.DB_DATABASE,
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD
     })
   }
-  return instance;
+  return sql;
 }
