9cc58fd02331415f170d2c6356ad981f352ebce8
LucaIgnatescu <lucaignatescu003@gmail.com>
2024-05-24 17:13:11 +0300

    refactored github auth

--- src/app/auth/callbacks/github/route.tsx
+++ src/app/auth/callbacks/github/route.tsx
@@ -1,7 +1,41 @@
 import { redirect } from "next/navigation";
 import { NextRequest } from "next/server";
 
+type GitHubToken = {
+  access_token: string,
+  expires_in: number,
+  refresh_token: string,
+  refresh_token_expires_in: number,
+  token_type: string,
+  scope: string
+}
+
 export async function GET(req: NextRequest) {
-  console.log("hit get request");
-  redirect('/');
+
+  const params = req.nextUrl.searchParams;
+  const code = params.get("code");
+
+  if (!code) { // TODO: Clearly signal an error has occured
+    redirect('/auth');
+  }
+
+  console.log(code);
+  const res = await fetch(
+    "https://github.com/login/oauth/access_token?" + new URLSearchParams({
+      client_id: process.env.GITHUB_CLIENT!,
+      client_secret: process.env.GITHUB_SECRET!,
+      code: code,
+      // redirect_uri: "localhost:3000"
+    }), {
+    method: "POST",
+    headers: {
+      Accept: "application/vnd.github+json"
+    },
+    cache: "no-store",
+  }).then(res => res.json()).catch(err => redirect('/auth')) as GitHubToken;
+
+  const { access_token, refresh_token } = res;
+
+  console.log(access_token, refresh_token);
+  redirect('/auth');
 }
--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,9 +1,12 @@
+import { redirect } from "next/navigation";
+
 async function LoginBtn() {
   return <div className="bg-green-50 text-center text-black">
     <form action={async () => {
       "use server";
       const redirectURL = `https://github.com/login/oauth/authorize?` +
         `client_id=${process.env.GITHUB_CLIENT}`;
+      redirect(redirectURL);
     }}>
       <input type="submit" value={"Log In with GitHub"} />
     </form>
